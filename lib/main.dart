3b977aae77171478330e3a2c8049d27c423fdde4
Ammar Al Khatib <ammar.mngr@gmail.com>
2020-12-20 17:34:04 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: Text('تطابق صورة'),
+        backgroundColor: Colors.indigo[800],
+      ),
+      body: ImagePage(),
+    ),
+  ));
+}
+
+class ImagePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
